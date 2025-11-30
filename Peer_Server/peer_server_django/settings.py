9f1284bcdec2923d4784f8335b96cf2d54a6028b
Rui  Carvalho <95130928+ruicarvalho1@users.noreply.github.com>
2025-11-30 19:00:42 +0000

    Fix Dotenv

--- Peer_Server/peer_server_django/settings.py
+++ Peer_Server/peer_server_django/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -84,13 +87,6 @@ DATABASES = {
     }
 }
 
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 
 # Password validation
